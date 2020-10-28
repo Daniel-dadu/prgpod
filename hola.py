1c148da848f803e61a7e5fc2cec1f73280f6ff9c
Daniel-dadu <dadu9494@gmail.com>
2020-10-28 00:44:22 +0000

    Primeros pasos con pandas

---
+++ hola.py
@@ -0,0 +1,35 @@
+# %%
+import numpy as np
+import pandas as pd
+
+lista = [1,2,4,6]
+nplista = np.array(lista)
+pdlista = pd.Series(lista)
+
+print(lista*2)
+print(nplista*2)
+print(pdlista*2)
+
+# %%
+
+df = pd.read_csv("insurance.csv")
+
+print(df)
+
+# %%
+#Se le puede poner entre parentesis cuantos elementos quieres imprimir
+df.head(5) 
+
+df.tail()
+df.sample()
+
+df.describe()
+df.info()
+
+# %%
+df[['age']]
+
+
+#df[10]
+
+# %%
